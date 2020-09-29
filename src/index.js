f81273a718bbffc9c3be34c527349d086c9b79c8
Andrey Lisovskiy <andrey.lisovskiy@intrahealth.com>
2020-09-29 13:20:34 +0300

    feat: implement Human Readable Number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const zero = 'zero';
+const hundred = 'hundred';
+const numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const hundreds = ['', 'hundred', 'thousand', 'million'];
+
+function encode(n100 = 0, n10 = 0, n1 = 0, rank) {
+    let res = [];
+    if (n100) res.push(`${numbers[n100]} ${hundred}`);
+    if (n10 === 1) {
+        res.push(numbers[n10 * 10 + n1]);
+    } else {
+        res.push(tens[n10]);
+        res.push(numbers[n1]);
+    }
+    res.push(rank);
+    return res.filter((value)=>value).join(' ').trim();
+}
+
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return zero;
+    let res = '';
+    let digits = String(number).split('').reverse();
+    for (let i = Math.ceil(digits.length / 3) - 1; i >= 0; i --) {
+        res += encode(+digits[i * 3 + 2] || 0, +digits[i * 3 + 1] || 0, +digits[i * 3] || 0, hundreds[i]) + ' ';
+    }
+    return res.trim();
 }
